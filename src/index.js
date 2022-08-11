2bcb8110cabe83fff447c94e6ebf6a4a91cbc56f
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2022-08-12 01:22:45 +0200

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
+  let bracketPairs = {};
+  let closedBrackets = [];
+
+  for (let item of bracketsConfig) {
+    bracketPairs[item[1]] = item[0];
+    closedBrackets.push(item[1]);
+  };
+
+  console.log(bracketPairs, closedBrackets)
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+
+    if (closedBrackets.indexOf(current) > -1) {
+      if (bracketPairs[current] != stack.pop()) return false;
+     } else {
+        stack.push(current);
+      }
+  }
+
+  return (stack.length == 0);
+
   // your solution
 }
